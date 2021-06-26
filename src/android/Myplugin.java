a1addff551c4be6086348ed12c66c655467f7696
nicholaslopiccolo <nicholas.lopiccolo1998@gmail.com>
2021-06-26 12:02:50 +0200

    feat: log level & task

--- src/android/Myplugin.java
+++ src/android/Myplugin.java
@@ -3,45 +3,87 @@ package cordova.plugin.myplugin;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+//import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+//import org.json.JSONObject;
+
+import jdk.internal.net.http.common.Log;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class Myplugin extends CordovaPlugin {
-    private final String COOL_METHOD = "coolMethod";
-    private final String ECHO = "echo";
+    private static final  String ECHO = "echo";
+    private static final String NEW_TASK = "newTask";
+    private static final String DEL_TASK = "delTask";
 
+    private ArrayList <Timer> alt = new ArrayList<>();
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals(COOL_METHOD)) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        } else if(action.equals(ECHO)){
+        if (action.equals(ECHO)) {
             String message = args.getString(0);
             this.echo(message, callbackContext);
             return true;
+        } 
+        else if (action.equals(NEW_TASK)) {
+            this.newTask(callbackContext);
+            return true;
+        } 
+        else if (action.equals(DEL_TASK)) {
+            this.delTask(callbackContext);
+            return true;
         }
+        Log.i("tag","no function called");
         return false;
-    }
-
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message); // chiami la callback SUCCESS via javascript
-        } else {
-            callbackContext.error("Expected one non-empty string argument."); // chiami la callback ERROR via javascript
-        }
     }
 
     private void echo(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
-            callbackContext.success(message); // la callback SUCCESS via javascript
+            Log.i("tag","LOG: " + message);
+            callbackContext.success("LOLLO: " + message); // la callback SUCCESS via javascript
         } else {
+            Log.i("tag","error echo");
             callbackContext.error("Expected one non-empty string argument."); // la callback ERROR via javascript
+        }
+    }
+
+    private void newTask(CallbackContext callbackContext) {
+        try {
+            Timer t = new Timer();
+            TimerTask ts = new TimerTask() {
+                @Override
+                public void run() {
+                    Log.i("tag", "A Kiss every 5 seconds");
+                    //System.out.println("A Kiss every 5 seconds");
+                }
+            };
+
+            t.scheduleAtFixedRate(ts, 0, 5000);
+
+            alt.add(t);
+            callbackContext.success("created task"); // la callback SUCCESS via javascript
+        } catch (Exception e) {
+            Log.i("tag","error in new task");
+            callbackContext.error("error: "+e.getMessage()); // la callback ERROR via javascript
+
+        }
+    }
+
+    private void delTask(CallbackContext callbackContext) {
+        try {
+            alt.remove(0);
+            Log.i("tag","task removed");
+            callbackContext.success("removed task"); // la callback SUCCESS via javascript
+        } catch (Exception e) {
+            Log.i("tag","error in del task");
+            callbackContext.error("error: "+e.getMessage()); // la callback ERROR via javascript
+
         }
     }
 }
--- www/Myplugin.js
+++ www/Myplugin.js
@@ -1,10 +1,13 @@
 var exec = require('cordova/exec');
 
 module.exports =  {
-    coolMethod(args, success, error) {
-        exec(success, error, 'Myplugin', 'coolMethod', [args]);
-    },
     echo(args,success,error){
         exec(success, error, 'Myplugin', 'echo', [args]);
+    },
+    newTask(args, success, error) {
+        exec(success, error, 'Myplugin', 'newTask', [args]);
+    },
+    delTask(args, success, error) {
+        exec(success, error, 'Myplugin', 'delTask', [args]);
     }
 }
